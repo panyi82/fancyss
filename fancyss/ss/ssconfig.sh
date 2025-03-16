18fdf2e846f7e124e2f46f43124a8fff642ccfdf
panyi82 <panyi82@gmail.com>
2025-03-15 22:41:06 -0700

    Update ssconfig.sh

--- fancyss/ss/ssconfig.sh
+++ fancyss/ss/ssconfig.sh
@@ -1972,9 +1972,9 @@ start_dns_old() {
 	# 8. direct
 	if [ "${ss_foreign_dns}" == "8" ]; then
 		if [ "${ss_basic_mode}" == "6" ]; then
-  			local china_dns_user = "114.114.114.114"
+  			ss_china_dns_user = "114.114.114.114"
 			echo_date "回国模式，国外DNS采用直连方案。${ss_china_dns_user}"
-   			start_dns2socks ${china_dns_user} 7913 0
+   			start_dns2socks ${ss_china_dns_user} 7913 0
 		else
 			echo_date "非回国模式，国外DNS直连解析不能使用，自动切换到dns2socks方案。"
 			dbus set ss_foreign_dns=3
